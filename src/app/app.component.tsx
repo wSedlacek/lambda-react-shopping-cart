05e718daa576a848a9388b7bd457d83e0b59f6c9
William Sedlacek <wsedlacekc@gmail.com>
2019-10-07 14:38:32 -0500

    Setup addItem
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -8,11 +8,11 @@ import { Products } from './components/products/products.component';
 import { ShoppingCart } from './components/shopping-cart/shopping-cart.component';
 
 function App() {
-  const [products] = React.useState(data);
-  const [cart, setCart] = React.useState([]);
+  const [products] = React.useState<Book[]>(data);
+  const [cart, setCart] = React.useState<Book[]>([]);
 
   const addItem = (item: Book) => {
-    // add the given item to the cart
+    setCart([...cart, item]);
   };
 
   return (
